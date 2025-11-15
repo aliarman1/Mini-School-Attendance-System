5a2c65dd29a337f7a80300f29c2cc788c8b66097
Ali <aliarman45039@gmail.com>
2025-11-15 20:44:21 +0600

    scaffold done

---
+++ attendance-backend/app/Console/Commands/AttendanceGenerateReport.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AttendanceGenerateReport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:attendance-generate-report';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
